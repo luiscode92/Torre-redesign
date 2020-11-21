cd03ee6a015a0cb86a0591352cdcfa4f872b5617
Luis Sanjuan <luiscode92@gmail.com>
2020-11-21 11:14:21 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>here will be the redesing</h1>
+    </div>
+  );
+}
+
+export default App;
